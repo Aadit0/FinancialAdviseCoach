e6673142a83225a469e993d392780fd1d7e2fb76
Mo Babiker <mobabiker127@gmail.com>
2025-10-26 01:33:24 +0100

    setting up basic frontend, backend environment

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,7 @@
 import os
 
+from flask import Flask
+
 from groq import Groq
 
 from dotenv import load_dotenv, find_dotenv
@@ -7,6 +9,15 @@ from dotenv import load_dotenv, find_dotenv
 from pathlib import Path
 
 load_dotenv(Path(".env"))
+
+app = Flask(__name__)
+
+@app.route("/members")
+def members():
+    return {"members": ["Member1", "Member2", "Member3"]}
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 client = Groq(
     api_key=(os.getenv("GROQ_API_KEY")),
@@ -22,4 +33,4 @@ chat_completion = client.chat.completions.create(
     model="llama-3.3-70b-versatile",
 )
 
-print(chat_completion.choices[0].message.content)
+# print(chat_completion.choices[0].message.content)
